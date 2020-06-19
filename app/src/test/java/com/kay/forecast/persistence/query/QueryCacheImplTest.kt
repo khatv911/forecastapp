705bbc02d3e6aa1ebcce68bec27b1da98bd49710
Kay Tran <khatv911@gmail.com>
2020-06-19 15:51:56 +0800

    remove runner

--- app/src/test/java/com/kay/forecast/persistence/query/QueryCacheImplTest.kt
+++ app/src/test/java/com/kay/forecast/persistence/query/QueryCacheImplTest.kt
@@ -11,8 +11,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 
-@RunWith(AndroidJUnit4::class)
-@Config(sdk = [28], application = App::class)
 class QueryCacheImplTest {
 
 
