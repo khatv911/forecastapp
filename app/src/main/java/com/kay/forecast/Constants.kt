ca3f176922f3af9c35a75b3fd3203f29a5d80977
Kay Tran <khatv911@gmail.com>
2020-06-20 11:06:48 +0800

    correct pin

--- app/src/main/java/com/kay/forecast/Constants.kt
+++ app/src/main/java/com/kay/forecast/Constants.kt
@@ -5,6 +5,6 @@ const val NUM_OF_FORECASTS = 16 // maximum
 const val DEBOUNCE_DURATION = 900L
 // stores these in constants for brevity
 const val API_URL = "api.openweathermap.org"
-const val PIN_SHA256 = "vRU+17BDT2iGsXvOi76E7TQMcTLXAqj0+jGPdW7L1vM="
+const val PIN_SHA256 = "sha256/x4QzPSC810K5/cMjb05Qm4k3Bw5zBn4lTdO/nEW/Td4="
 const val BASE_URL = "https://$API_URL"
 const val APP_ID = "60c6fbeb4b93ac653c492ba806fc346d"
