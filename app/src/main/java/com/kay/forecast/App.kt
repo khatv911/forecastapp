0a5dc2077f89a01fd519d6873c837e6dcfc8575d
Kay Tran <khatv911@gmail.com>
2020-06-20 09:17:04 +0800

    no setter for dependencies container

--- app/src/main/java/com/kay/forecast/App.kt
+++ app/src/main/java/com/kay/forecast/App.kt
@@ -6,6 +6,8 @@ import timber.log.Timber
 class App : Application() {
 
     lateinit var dependenciesContainer: DependenciesContainer
+        private set
+
     override fun onCreate() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
